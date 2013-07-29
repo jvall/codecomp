26a8c46ac7234c4e2d0414ff11189f2bdc4c8019
John <qandhomer@gmail.com>
2013-07-28 22:11:34 -0400

    Updated UI, fixed problems with collapse

--- site/js/problemsctrl.js
+++ site/js/problemsctrl.js
@@ -8,9 +8,16 @@ app.controller('ProblemsCtrl', function($scope) {
         },
         {
             title: "Tukey Ninether",
-            score: 0,
+            score: 100,
             description: "Take your entire life to get this problem working",
             isCollapsed: true
         }
     ];
-})
+
+    $scope.probClass = function(prob) {
+        if (prob.score === 100)
+            return "sliding-header-complete";
+        else
+            return "sliding-header";
+    };
+});
